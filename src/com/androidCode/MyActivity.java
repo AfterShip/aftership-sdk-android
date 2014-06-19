2b015a0165f91e684b5164ab6024c92432b58540
jesus- <chusma.ellagarto@gmail.com>
2014-06-19 12:09:38 +0800

    first example

--- src/com/androidCode/MyActivity.java
+++ src/com/androidCode/MyActivity.java
@@ -4,6 +4,7 @@ import Enums.ConnectionAPIMethods;
 import android.app.Activity;
 import android.os.Bundle;
 import Classes.*;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
@@ -30,34 +31,55 @@ public class MyActivity extends Activity implements AsyncTaskCompleteListener<Co
     }
 
     public void onTaskComplete(ConnectionAPI result) {
-        try {
-            switch (result.getMethod().getNumberMethod()) {
-                case 0://getLastCheckpoint
-                    break;
-                case 1: //reactivate
-                    break;
-                case 2://getTrackingByNumber
-                    break;
-                case 3://getTracking
-                    break;
-                case 4://deleteTracking
-                    break;
-                case 5://postTracking
-                    break;
-                case 6://putTracking
-                    break;
-                case 7://getCouriers(7)
-                    List<Courier> couriers = result.getCouriersReturn();
-                    for(int i=0;i<couriers.size();i++)
-                        tv_main.setText(tv_main.getText() + couriers.get(i).getName() +"\t"+couriers.get(i).getSlug()+"\n");
-                    break;
-                case 8://detectCouriers(8)
-                    break;
-            }
-        }catch (Exception e){
-        // do something with the exception
-       }
+        if (result.getException()!=null)
+            System.out.println(result.getException().getMessage());//do something with the exception
 
+        switch (result.getMethod().getNumberMethod()) {
+            case 0://getLastCheckpoint
+                break;
+            case 1: //reactivate
+                break;
+            case 2://getTrackingByNumber
+                break;
+            case 3://getTracking
+                List<Tracking> trackings = result.getTrackingsReturn();
+                for (int i = 0; i < trackings.size(); i++)
+                    tv_main.setText(tv_main.getText() + trackings.get(i).getTrackingNumber() + "\t" + trackings.get(i).getSlug() + "\n");
+                break;
+            case 4://deleteTracking
+                break;
+            case 5://postTracking
+                break;
+            case 6://putTracking
+                break;
+            case 7://getCouriers(7)
+                List<Courier> couriers = result.getCouriersReturn();
+                for (int i = 0; i < couriers.size(); i++)
+                    tv_main.setText(tv_main.getText() + couriers.get(i).getName() + "\t" + couriers.get(i).getSlug() + "\n");
+                break;
+            case 8://detectCouriers(8)
+                break;
+        }
+    }
+    public void  onBtnClickedNumber1(View view){
+            tv_main.setText("");
 
     }
+    public void  onBtnClickedNumber2(View view){
+        try {
+            new ConnectionAPI(API_KEY, ConnectionAPIMethods.getCouriers, this).execute();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public void  onBtnClickedNumber3(View view){
+        try {
+            new ConnectionAPI(API_KEY, ConnectionAPIMethods.getTracking, this,1).execute();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
 }
